da6e56b667f4e68649f78be7238ea25b1be32c10
Albert Chong <kickchong@gmail.com>
2025-02-06 14:50:00 -0800

    add IMM

--- ecr.tf
+++ ecr.tf
@@ -2,7 +2,7 @@
 
 resource "aws_ecr_repository" "my_ecr_repo" {
   name                 = var.appname
-  image_tag_mutability = "MUTABLE"  # or "IMMUTABLE" based on your requirement
+  image_tag_mutability = "IMMUTABLE"  # or "IMMUTABLE" based on your requirement
   image_scanning_configuration {
     scan_on_push = true
   }
