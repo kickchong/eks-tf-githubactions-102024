2f99f9fc088c716c00dadf0e4b88f08e54970a96
Albert Chong <kickchong@gmail.com>
2025-02-06 15:49:26 -0800

    v3

--- vpc.tf
+++ vpc.tf
@@ -76,9 +76,27 @@ resource "aws_s3_bucket" "vpc_flow_logs" {
     enabled = true
   }
 
+  logging {
+        target_bucket = "terraform-albert1"
+    }
+
   lifecycle {
     prevent_destroy = true
   }
+
+  server_side_encryption_configuration {
+     rule {
+       apply_server_side_encryption_by_default {
+         kms_master_key_id = "arn"
+         sse_algorithm     = "aws:kms"
+       }
+     }
+   }
+}
+
+resource "aws_s3_bucket_public_access_block" "vpc_flow_logs" {
+  bucket = aws_s3_bucket.vpc_flow_logs.id
+  block_public_acls = true
 }
 
 resource "aws_flow_log" "appsec-preprod" {
